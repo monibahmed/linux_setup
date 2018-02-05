a95eb704b7557573f4f3a3eebb66259d0b0dc173
Monib Ahmed <monibahmed@gmail.com>
2018-02-05 12:25:06 -0800

    added .emacs.d

---
+++ .emacs.d/elpa/twilight-bright-theme-20130605.143/twilight-bright-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "twilight-bright-theme" "20130605.143" "A Emacs 24 faces port of the TextMate theme" 'nil :commit "322157cb2f3bf7920ecd209dafc31bc1c7959f49" :url "https://github.com/jimeh/twilight-bright-theme.el" :keywords '("themes"))
